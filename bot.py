80d592e9eb673f0a9027aa29b6e60f19f0da21f9
acon <101356372+acornitum@users.noreply.github.com>
2024-06-22 12:49:34 -0400

    [playing around] got it to respond to itself in thread

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-import slack
+import slack_sdk
 import os
 from pathlib import Path
 from dotenv import load_dotenv 
@@ -6,6 +6,22 @@ from dotenv import load_dotenv
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
 
-client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
+chan = '#acon-bot-testing' # channel
 
-client.chat_postMessage(channel='#acon-yaps', text="bzzz...bzz...testing...")
+client = slack_sdk.WebClient(token=os.environ['SLACK_TOKEN'])
+
+client.chat_postMessage(channel=chan, text="bzzz...bzz...testing...")
+
+response = client.chat_postMessage(
+    channel=chan,
+    thread_ts="1476746830.000003",
+    text="Hello from your app! :tada:",
+)
+
+times = response["ts"]
+
+client.chat_postMessage(
+    channel=chan,
+    thread_ts=times,
+    text="Hello from your app! :tada:",
+)
